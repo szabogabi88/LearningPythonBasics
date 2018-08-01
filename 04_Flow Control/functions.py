3047096466e5e4ef6124152c924c121ab4f41498
szabogabi88 <szabogabi88@gmail.com>
2018-08-01 23:41:04 +0200

    comment of fib extended

--- 04_Flow Control/functions.py
+++ 04_Flow Control/functions.py
@@ -2,6 +2,7 @@
 def fib(n):
 	""" This is a docstring of function fib()"""
 	""" Just type \'from <name of this file without extension .py\' import fib"""
+	""" Then call e.g. fib(10) in python interpreter"""
 	a=0
 	b=1
 	while a<n:
